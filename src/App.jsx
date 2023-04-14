bd4c95dab8f8cd31ee2dbcef1748513e9a3f3461
Rodrigo Rodrigues <rodrigo.rodrigues.live@hotmail.com>
2023-04-14 14:04:35 -0300

    Study day 14/04/20232 14:03

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header"
+import './styles.css'
 
 export function App() {  
   return (
-    //<h1>Hello World</h1>
     <div>
+      <Header />
       <Post 
         author="Rodrigo Rodrigues" 
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt ullam voluptatibus, quod, veritatis aperiam nulla cupiditate consectetur tempora sequi perspiciatis deserunt reiciendis quisquam iure facere, repudiandae non quis assumenda error!" 
